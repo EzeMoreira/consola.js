9268cd4d4b56e91917f8bfe21076d57286cf17dd
EzequielMoreira <eezequielmoreira@gmail.com>
2025-01-09 20:49:18 -0300

    add ej js

---
+++ codigo.js
@@ -0,0 +1,8 @@
+//          Ejemplos de console.log
+
+console.log("Hola que tal");
+console.log(5 + 10);
+
+//Aqui simplificamos en una sola linea estas dos funciones que le pedimos
+
+console.log("El resultado de la siguiente operacion es: ", 5 + 10);
