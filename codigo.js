240699cfaa0256c31dc161e15c6d7b11641bc5e6
EzequielMoreira <eezequielmoreira@gmail.com>
2025-01-09 21:04:21 -0300

    add ej de message error

--- codigo.js
+++ codigo.js
@@ -6,3 +6,15 @@ console.log(5 + 10);
 //Aqui simplificamos en una sola linea estas dos funciones que le pedimos
 
 console.log("El resultado de la siguiente operacion es: ", 5 + 10);
+
+
+
+//          Ejemplos de mensajes de error
+
+console.error("El ejercicio que intentas hacer esta mal");
+
+console.warn("Esta es una advertencia, práctica mas");
+
+console.info("Aquí aclaramos un texto que quieramos indicar al trabajar");
+
+console.debug("Este método se utiliza para registrar mensajes de depuracion de codigo, su proposito es que muestre mensajes utiles durante el desarrollo y depuracion del codigo");
